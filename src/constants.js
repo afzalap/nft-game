5356de07a3fefcde488deecde97afc450f4f225b
Afzal Ahmed Pinjar <afzalap163@gmail.com>
2021-11-12 17:57:02 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xa06C94B7171198aCf2fa5362460B2f1AFC1C6D18';
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
